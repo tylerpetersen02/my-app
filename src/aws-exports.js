6d02e7b2617a485af88735d435eed7c01a7d78c5
Tyler Petersen <tylerpetersen02@gmail.com>
2022-11-09 10:34:36 -0800

    updated score buttons

---
+++ src/aws-exports.js
@@ -0,0 +1,8 @@
+const awsmobile =  {
+  "aws_appsync_graphqlEndpoint": "https://jixpde5kena2pk3py6kb36k5c4.appsync-api.us-east-2.amazonaws.com/graphql",
+  "aws_appsync_region": "us-east-2",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": "da2-eww5fx3hzfe4hawgcerbx6vyty",
+};
+
+export default awsmobile;
